06815bb76d888b2fbb1bb28b104738f077b82211
Lauren Boone <lboone2094@gmail.com>
2020-04-13 12:31:03 -0600

    add class list page

--- lib/Screens/Teacher/teacherclasslist.dart
+++ lib/Screens/Teacher/teacherclasslist.dart
@@ -1,39 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ClassListPage extends StatelessWidget{
   @override
 
   Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Classes")
-      ),
-      body: ListView(
-          children:  <Widget>[
-            Card(child: ListTile(
-              title: Text('Classes'),
-              subtitle: Text('View All Classes'),
-              trailing: Icon(Icons.arrow_forward_ios), 
-              onTap: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ClassListPage(),
-                ),
-                );
-              }
+    return new StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('Teachers').snapshots(),
+      builder:(BuildContext conetext, AsyncSnapshot<QuerySnapshot> snapshot){
+        if(!snapshot.hasData)return new Text('..Loading');
+        return new ListView(
+          children: snapshot.data.documents.map((document){
+            return new ListTile( 
+              title: new ListTile(
+                title: new Text(document['name']),
+
               ),
-              ),
-          ]
-          ),
-        
-       floatingActionButton: RaisedButton(
-          onPressed: (){
-            Navigator.pop(context);
-          },
-          child: Text("Go Back"),
-          
-        ),
-      
-        );
+            );
+            
+          }).toList(),
+          );
+      }
+
+    );
   }
 }
