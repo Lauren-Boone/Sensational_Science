1e761bfd83e5a87a1ba36dfda48bc2af3a32980e
Lauren Boone <lboone2094@gmail.com>
2020-04-13 15:19:20 -0600

    Edited formatting

--- lib/Screens/Teacher/teacherclasslist.dart
+++ lib/Screens/Teacher/teacherclasslist.dart
@@ -13,25 +13,31 @@ getClassList(String teachName){
 }
 
   Widget build(BuildContext context){
-    return Material(
-          child: new StreamBuilder<QuerySnapshot>(
-        stream: getClassList("Dr. Who"),
-        
-        builder:(BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
-          if(!snapshot.hasData)return new Text('..Loading');
-          return new ListView(
-            children: snapshot.data.documents.map((document){
-              return new ListTile( 
-                title: new ListTile(
-                  title: new Text(document['name']),
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Classes")
+      ),
+          body: Material(
+            child: new StreamBuilder<QuerySnapshot>(
+          stream: getClassList("Dr. Who"),
+          
+          builder:(BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
+            if(!snapshot.hasData)return new Text('..Loading');
+            return new ListView(
+              children: snapshot.data.documents.map((document){
+                return new ListTile( 
+  
+                    title: new Text(document['name']),
 
-                ),
+                  
+                );
+                
+              }).toList(),
               );
-              
-            }).toList(),
-            );
-        }
+          }
 
+        ),
       ),
     );
     
