f3527d75acc0b63f4b46dbfd90c17090da551f23
Lauren Boone <lboone2094@gmail.com>
2020-04-13 15:12:54 -0600

    Fixed Teacher Home page

--- lib/Screens/Teacher/teachermain.dart
+++ lib/Screens/Teacher/teachermain.dart
@@ -1,32 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sensational_science/Screens/Teacher/teacherclasslist.dart';
 
-class ClassListPage extends StatelessWidget{
+
+class TeacherHome extends StatelessWidget{
   @override
 
   Widget build(BuildContext context){
-    return Material(
-          child: new StreamBuilder<QuerySnapshot>(
-        stream: Firestore.instance.collection('Teachers').snapshots(),
-        builder:(BuildContext conetext, AsyncSnapshot<QuerySnapshot> snapshot){
-          if(!snapshot.hasData)return new Text('..Loading');
-          return new ListView(
-            children: snapshot.data.documents.map((document){
-              return new ListTile( 
-                title: new ListTile(
-                  title: new Text(document['name']),
-
-                ),
-              );
-              
-            }).toList(),
-            );
-        }
-
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home")
       ),
-    );
-    
+      body: ListView(
+          children:  <Widget>[
+            Card(child: ListTile(
+              title: Text('Classes'),
+              subtitle: Text('View All Classes'),
+              trailing: Icon(Icons.arrow_forward_ios), 
+              onTap: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>ClassListPage(),
+                  ),
+                );
+              },
+              ),
+              ),
+          ]
+          ),
+        
+       floatingActionButton: RaisedButton(
+          onPressed: (){
+            Navigator.pop(context);
+          },
+          child: Text("Go Back"),
+          
+        ),
+      
+        );
   }
 }
-
-
