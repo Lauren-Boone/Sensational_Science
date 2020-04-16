09db8abdafdbc5ea35ed05511b15da9b0d7e2760
Abbi Devins-Suresh <aesther2012@gmail.com>
2020-04-16 17:53:02 -0400

    added more substance to splash page

--- lib/Screens/Login/splash.dart
+++ lib/Screens/Login/splash.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:sensational_science/main.dart';
+import '../Teacher/teachermain.dart';
+import 'login.dart';
 import 'dart:async';
 
 final FirebaseAuth authenticate = FirebaseAuth.instance; 
@@ -16,50 +18,46 @@ class SplashScreen extends StatefulWidget {
 }
 
 class SplashScreenState extends State<SplashScreen> {
-  final _formKey = GlobalKey<FormState>();
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController(); 
-
-  findUser(idUser) async {
-    var data = Firestore.instance.collection('Teachers').document(idUser).get();
-    await data.then((user) {
-      return user.exists;
-    });
-  }
-
-  getUserData(String idUser) async {
-    var data = Firestore.instance.collection('Teachers').document(idUser).get();
-    return await data.then((pw) {
-      if (!pw.exists) {
-        return null;
-      }
-    });
-  }
-
   @override
+  getUserState(){
+    FirebaseAuth.instance
+      .currentUser()
+      .then((user) => {
+        if(user == null){
+          Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context)=> LoginAccount())
+              )
+        }else{
+          Firestore.instance
+          .collection("Teachers")
+          .document(user.uid)
+          .get()
+          .then((DocumentSnapshot result) => 
+            Navigator.push(
+              context, 
+              MaterialPageRoute(
+                builder:(context) => TeacherHome(
+                  
+                )
+              )
+            )
+          )
+          .catchError((err)=> print(err))
+        }
+      });
+  }
+@override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text("Login to Account"),
-        ),
-        backgroundColor: Colors.lightBlue[100], 
-        body: Form(
-            key: _formKey,
-            child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Column(children: <Widget>[
-                  //Text form fields and raised button
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16.0),
-                    child: Text(
-                      'Sensational Science Projects App', 
-                      textAlign: TextAlign.center, 
-                      style: TextStyle(fontWeight: FontWeight.bold),
-
-                    )
-                  )
-                ]))));
+      appBar:AppBar(
+        title: Text('Sensational Science Projects'),
+      ),
+      backgroundColor: Colors.lightGreen,
+      body:Center(
+        child: Text('inspired by Citizen Science Projects'))
+      );
   }
+
 }
+
