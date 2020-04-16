02dd7b3573666b8fa40aa4410ebd52b0807e0240
Abbi Devins-Suresh <aesther2012@gmail.com>
2020-04-16 10:14:43 -0400

    firestore addition

--- lib/Screens/Login/login.dart
+++ lib/Screens/Login/login.dart
@@ -3,6 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginAccount extends StatefulWidget {
+  LoginAccount({Key key}) : super(key: key);
+
   @override
   LoginAccountState createState() {
     return LoginAccountState();
@@ -11,6 +13,23 @@ class LoginAccount extends StatefulWidget {
 
 class LoginAccountState extends State<LoginAccount> {
   final _formKey = GlobalKey<FormState>();
+  final loginController = TextEditingController();
+
+  findUser(idUser) async {
+    var data = Firestore.instance.collection('users').document(idUser).get();
+    await data.then((user) {
+      return user.exists;
+    });
+  }
+
+  getUserData(String idUser) async {
+    var data = Firestore.instance.collection('users').document(idUser).get();
+    return await data.then((doc) {
+      if (!doc.exists) {
+        return null;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,24 +41,26 @@ class LoginAccountState extends State<LoginAccount> {
         ),
         body: Form(
             key: _formKey,
-            child: Column(children: <Widget>[
-              //Text form fields and raised button
-              TextFormField(
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-              RaisedButton(
-                  onPressed: () {
-                    if (_formKey.currentState.validate()) {
-                      Scaffold.of(context).showSnackBar(
-                          SnackBar(content: Text('Processing Data')));
-                    }
-                  },
-                  child: Text('Submit'))
-            ])));
+            child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(children: <Widget>[
+                  //Text form fields and raised button
+                  TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                  ),
+                  RaisedButton(
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          Scaffold.of(context).showSnackBar(
+                              SnackBar(content: Text('Processing Data')));
+                        }
+                      },
+                      child: Text('Submit'))
+                ]))));
   }
 }
