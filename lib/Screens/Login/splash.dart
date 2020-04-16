f1cbb375e6658aa56dd95c0164f96b39a39c99f2
Abbi Devins-Suresh <aesther2012@gmail.com>
2020-04-16 17:29:40 -0400

    starting to add in material for splash screen

---
+++ lib/Screens/Login/splash.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:sensational_science/main.dart';
+import 'dart:async';
+
+final FirebaseAuth authenticate = FirebaseAuth.instance; 
+
+class LoginAccount extends StatefulWidget {
+  LoginAccount({Key key}) : super(key: key);
+
+  @override
+  LoginAccountState createState() {
+    return LoginAccountState();
+  }
+}
+
+class LoginAccountState extends State<LoginAccount> {
+  final _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController(); 
+
+  findUser(idUser) async {
+    var data = Firestore.instance.collection('Teachers').document(idUser).get();
+    await data.then((user) {
+      return user.exists;
+    });
+  }
+
+  getUserData(String idUser) async {
+    var data = Firestore.instance.collection('Teachers').document(idUser).get();
+    return await data.then((pw) {
+      if (!pw.exists) {
+        return null;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text("Login to Account"),
+        ),
+        backgroundColor: Colors.lightBlue[100], 
+        body: Form(
+            key: _formKey,
+            child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(children: <Widget>[
+                  //Text form fields and raised button
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: Text(
+                      'Sensational Science Projects App', 
+                      textAlign: TextAlign.center, 
+                      style: TextStyle(fontWeight: FontWeight.bold),
+
+                    )
+                  )
+                ]))));
+  }
+}
