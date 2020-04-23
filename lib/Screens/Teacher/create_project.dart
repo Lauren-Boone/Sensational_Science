b2dcb5547f1aff16b40bf4d487b873f9122894d6
Abbi Devins-Suresh <aesther2012@gmail.com>
2020-04-23 14:15:40 -0400

    starting to reorganize widgets and files

--- lib/Screens/Teacher/create_project.dart
+++ lib/Screens/Teacher/create_project.dart
@@ -5,31 +5,6 @@ import 'package:geolocator/geolocator.dart';
 import 'package:intl/intl.dart';
 import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 
-class BasicDateField extends StatefulWidget {
-  @override
-  BasicDateFieldState createState() => BasicDateFieldState();
-}
-
-class BasicDateFieldState extends State<BasicDateField> {
-  final format = DateFormat("yyyy-MM-dd");
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Text('Basic Date Field (${format.pattern})'),
-      DateTimeField(
-        format: format,
-        onShowPicker: (context, currentValue) {
-          return showDatePicker(
-              context: context,
-              firstDate: DateTime(1900),
-              initialDate: currentValue ?? DateTime.now(),
-              lastDate: DateTime(2100));
-        },
-      )
-    ]);
-  }
-}
-
 class CreateProject extends StatefulWidget {
   final String title;
   final bool pub;
---
+++ lib/Screens/Teacher/userlocation.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:intl/intl.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+
+Future<Position> getUserLocation() async {
+  try {
+    Position currentUserPosition = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print(currentUserPosition);
+    return currentUserPosition;
+  } catch (ex) {
+    Position currentUserPosition;
+    currentUserPosition = null;
+    print('Error getting user location');
+    return currentUserPosition;
+  }
+}
+
+class UserLocation extends StatefulWidget {
+  @override
+  UserLocationState createState() => UserLocationState();
+}
+
+class UserLocationState extends State<UserLocation> {
+  var results;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: new EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            new TextField(
+              //controller: controller,
+              decoration: new InputDecoration(
+                hintText: 'Location Description',
+              ),
+            ),
+            RaisedButton(
+              child: Text("Location"),
+              onPressed: () {
+                getUserLocation().then((result) {
+                  setState(() {
+                    results = result;
+                  });
+                });
+                print("Success!");
+              },
+            ),
+            new Text('$results')
+          ],
+        ));
+  }
+}
