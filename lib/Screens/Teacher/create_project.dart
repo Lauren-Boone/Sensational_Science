ba43315c5e5bf255c51f796a2a3bf9c26cd8a2c5
Abbi Devins-Suresh <aesther2012@gmail.com>
2020-04-27 20:32:07 -0400

    scrollability for lefthand side of create project

--- lib/Screens/Teacher/create_project.dart
+++ lib/Screens/Teacher/create_project.dart
@@ -121,157 +121,134 @@ class _CreateProjectState extends State<CreateProject> {
     final user = Provider.of<User>(context);
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-          title: Text("Create Project Details"),
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () => Navigator.pop(context, false),
-          )),
-      backgroundColor: Colors.grey[100],
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: new Expanded(
-          // child: Align(
-          child: new ListView(shrinkWrap: true, children: <Widget>[
-            new Container(
-              // child: SingleChildScrollView(
-              child: Row(children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    SingleChildScrollView(
-                      child: ConstrainedBox(
-                        constraints: BoxConstraints(
-                            minHeight:
-                                MediaQuery.of(context).size.height / 10 * 7,
-                            maxHeight:
-                                MediaQuery.of(context).size.height / 10 * 7),
-                        child: Column(
-                            // child: TestScrollView(),
-                            // width: MediaQuery.of(context).size.width / 4 * 2,
-                            // height: MediaQuery.of(context).size.height / 10 * 7,
-
-                            children: <Widget>[
-                              DragTarget(
-                                onWillAccept: (Widget addItem) {
-                                  if (addItem == null) {
-                                    return false;
-                                  }
-                                  return true;
-                                },
-                                onAccept: (Widget addItem) {
-                                  if (addItem.toString() ==
-                                      createTextInputHandler.toString()) {
-                                    controllers
-                                        .add(new TextEditingController());
-                                    acceptData.add(new TextInputItem(
-                                      controller: controllers[questionCount],
-                                    ));
-                                    questionCount++;
-                                    acceptType.add(addItem.toString());
-                                  } else if (addItem.toString() ==
-                                      createShortAnswer.toString()) {
-                                    controllers
-                                        .add(new TextEditingController());
-                                    acceptData.add(new ShortAnswerItem(
-                                      controller: controllers[questionCount],
-                                    ));
-                                    questionCount++;
-                                    acceptType.add(addItem.toString());
-                                  } else if (addItem.toString() ==
-                                      createNumericalInput.toString()) {
-                                    controllers
-                                        .add(new TextEditingController());
-                                    acceptData.add(new NumericalInputItem(
-                                      controller: controllers[questionCount],
-                                    ));
-                                    questionCount++;
-                                    acceptType.add(addItem.toString());
-                                  } else if (addItem.toString() ==
-                                      createImageCapture.toString()) {
-                                    controllers
-                                        .add(new TextEditingController());
-                                    acceptData.add(new AddImageInput(
-                                        controller:
-                                            controllers[questionCount]));
-                                    questionCount++;
-                                    acceptType.add(addItem.toString());
-                                  } else if (addItem.toString() ==
-                                      createLocationHandler.toString()) {
-                                    controllers
-                                        .add(new TextEditingController());
-                                    acceptData.add(new UserLocation(
-                                        controller:
-                                            controllers[questionCount]));
-                                    questionCount++;
-                                    acceptType.add(addItem.toString());
-                                  } else if (addItem.toString() ==
-                                      createMultipleChoice.toString()) {
-                                    controllers
-                                        .add(new TextEditingController());
-                                    answerControllers.add([]);
-                                    acceptData.add(new MultipleChoice(
-                                        controller: controllers[questionCount],
-                                        answers: answerControllers[
-                                            answerControllers.length - 1]));
-                                    questionCount++;
-                                    acceptType.add(addItem.toString());
-                                  }
-                                },
-                                builder: (context, List<dynamic> candidateData,
-                                    List<dynamic> rejectedData) {
-                                  return Container(
-                                    height: MediaQuery.of(context).size.height,
-                                    color: Colors.lightBlue[50],
-                                    child: acceptData.isEmpty
-                                        ? Center(
-                                            child: Text('Add Form Fields Here'),
-                                          )
-                                        : Column(children: acceptData),
-                                  );
-                                },
-                              ),
-                              Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 5.0),
-                                  child: RaisedButton(
-                                    child: Text('Submit Project'),
-                                    onPressed: () async {
-                                      widget.proj.addProjectDataToDoc(
-                                          user.uid,
-                                          controllers,
-                                          acceptType,
-                                          answerControllers,
-                                          questionCount,
-                                          widget.proj.getDocID());
-                                      widget.proj.addtodb(questionCount);
-                                      var answerCount = 0;
-                                      print('submit project onPressed');
-                                      for (var i = 0; i < questionCount; i++) {
-                                        print(acceptType[i] +
-                                            ': ' +
-                                            controllers[i].text);
-                                        if (acceptType[i] ==
-                                            createMultipleChoice.toString()) {
-                                          for (var item in answerControllers[
-                                              answerCount]) {
-                                            print(
-                                                'answer option: ' + item.text);
-                                          }
-                                          answerCount++;
-                                        }
-                                      }
-                                      Navigator.of(context).pop();
-                                      Navigator.of(context).pop();
-                                    },
-                                  ))
-                            ]),
+            appBar: AppBar(
+                title: Text("Create Project Details"),
+                leading: IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () => Navigator.pop(context, false),
+                )),
+            backgroundColor: Colors.grey[100],
+            body: Row(children: <Widget>[
+              Expanded(
+                child: ListView(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                padding: const EdgeInsets.all(8),
+                children: <Widget>[
+                  SingleChildScrollView(
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(
+                          maxHeight: MediaQuery.of(context).size.height, maxWidth: MediaQuery.of(context).size.width / 2),
+                      child: DragTarget(
+                        onWillAccept: (Widget addItem) {
+                          if (addItem == null) {
+                            return false;
+                          }
+                          return true;
+                        },
+                        onAccept: (Widget addItem) {
+                          if (addItem.toString() ==
+                              createTextInputHandler.toString()) {
+                            controllers.add(new TextEditingController());
+                            acceptData.add(new TextInputItem(
+                              controller: controllers[questionCount],
+                            ));
+                            questionCount++;
+                            acceptType.add(addItem.toString());
+                          } else if (addItem.toString() ==
+                              createShortAnswer.toString()) {
+                            controllers.add(new TextEditingController());
+                            acceptData.add(new ShortAnswerItem(
+                              controller: controllers[questionCount],
+                            ));
+                            questionCount++;
+                            acceptType.add(addItem.toString());
+                          } else if (addItem.toString() ==
+                              createNumericalInput.toString()) {
+                            controllers.add(new TextEditingController());
+                            acceptData.add(new NumericalInputItem(
+                              controller: controllers[questionCount],
+                            ));
+                            questionCount++;
+                            acceptType.add(addItem.toString());
+                          } else if (addItem.toString() ==
+                              createImageCapture.toString()) {
+                            controllers.add(new TextEditingController());
+                            acceptData.add(new AddImageInput(
+                                controller: controllers[questionCount]));
+                            questionCount++;
+                            acceptType.add(addItem.toString());
+                          } else if (addItem.toString() ==
+                              createLocationHandler.toString()) {
+                            controllers.add(new TextEditingController());
+                            acceptData.add(new UserLocation(
+                                controller: controllers[questionCount]));
+                            questionCount++;
+                            acceptType.add(addItem.toString());
+                          } else if (addItem.toString() ==
+                              createMultipleChoice.toString()) {
+                            controllers.add(new TextEditingController());
+                            answerControllers.add([]);
+                            acceptData.add(new MultipleChoice(
+                                controller: controllers[questionCount],
+                                answers: answerControllers[
+                                    answerControllers.length - 1]));
+                            questionCount++;
+                            acceptType.add(addItem.toString());
+                          }
+                        },
+                        builder: (context, List<dynamic> candidateData,
+                            List<dynamic> rejectedData) {
+                          return Container(
+                            height: MediaQuery.of(context).size.height,
+                            color: Colors.lightBlue[50],
+                            child: acceptData.isEmpty
+                                ? Center(
+                                    child: Text('Add Form Fields Here'),
+                                  )
+                                : Column(children: acceptData),
+                          );
+                        },
                       ),
                     ),
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    Padding(
+                  ),
+                  Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 5.0),
+                          child: RaisedButton(
+                            child: Text('Submit Project'),
+                            onPressed: () async {
+                              widget.proj.addProjectDataToDoc(
+                                  user.uid,
+                                  controllers,
+                                  acceptType,
+                                  answerControllers,
+                                  questionCount,
+                                  widget.proj.getDocID());
+                              widget.proj.addtodb(questionCount);
+                              var answerCount = 0;
+                              print('submit project onPressed');
+                              for (var i = 0; i < questionCount; i++) {
+                                print(
+                                    acceptType[i] + ': ' + controllers[i].text);
+                                if (acceptType[i] ==
+                                    createMultipleChoice.toString()) {
+                                  for (var item
+                                      in answerControllers[answerCount]) {
+                                    print('answer option: ' + item.text);
+                                  }
+                                  answerCount++;
+                                }
+                              }
+                              Navigator.of(context).pop();
+                              Navigator.of(context).pop();
+                            },
+                          ))
+                ],
+              ),
+              ),
+              Column(
+                children: <Widget>[
+                  Padding(
                       padding: EdgeInsets.all(10),
                       child: Column(
                         children: <Widget>[
@@ -339,15 +316,9 @@ class _CreateProjectState extends State<CreateProject> {
                             ),
                           ),
                         ],
-                      ),
-                    ),
-                  ],
-                ),
-              ]),
-            ),
-          ]),
-        ),
-      ),
-    ));
+                      )),
+                ],
+              )
+            ])));
   }
 }
