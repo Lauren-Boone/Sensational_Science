f76143c0e3708b5b40094c0fa81e4c9e019c9c6f
Abbi Devins-Suresh <aesther2012@gmail.com>
2020-04-30 09:33:16 -0400

    added in some widgets

--- lib/Screens/Teacher/viewproject.dart
+++ lib/Screens/Teacher/viewproject.dart
@@ -41,13 +41,12 @@ class _ViewProjectState extends State<ViewProject> {
   GetProject project;
   int _currentQuestion = 0;
   Future projectFuture;
-  
-  
+
   int _getType(_currentQuestion) {
-     //project.getdataFromProject();
+    //project.getdataFromProject();
     //setState(() {});
     //return project.getType(_currentQuestion);
-    switch(project.questions[_currentQuestion].type){
+    switch (project.questions[_currentQuestion].type) {
       case 'TextInputItem':
         return 0;
       case 'MultipleChoice':
@@ -55,32 +54,28 @@ class _ViewProjectState extends State<ViewProject> {
       case 'ShortAnswerItem':
         return 2;
       case 'UserLocation':
-        return 3; 
+        return 3;
     }
     return -1;
   }
-   
-Widget build(BuildContext context) {
 
-  return new MaterialApp(
-      
+  Widget build(BuildContext context) {
+    return new MaterialApp(
       home: new Scaffold(
           appBar: AppBar(title: Text("Random Widget")),
-          body: 
-          //project.questions.length == 0
+          body:
+              //project.questions.length == 0
 
-        //  ? Center(child: CircularProgressIndicator()
-          
-              
-         // )
-         // :
-          Center(child:
-      
-          FutureBuilder(
-              initialData: 0,
-              future: projectFuture,
-              builder: (context, snapshot) {
-              /*switch(snapshot.connectionState){
+              //  ? Center(child: CircularProgressIndicator()
+
+              // )
+              // :
+              Center(
+                  child: FutureBuilder(
+                      initialData: 0,
+                      future: projectFuture,
+                      builder: (context, snapshot) {
+                        /*switch(snapshot.connectionState){
                 case ConnectionState.waiting: 
                   return CircularProgressIndicator();
                 case ConnectionState.done:
@@ -89,18 +84,14 @@ Widget build(BuildContext context) {
 
               }*/
 
-                if(project.questions.length>0){
-                  return getQuestionWidget();
-               }
-              else{
-                  
-                  return CircularProgressIndicator();
-               }
-              }
-          )
-      )),
+                        if (project.questions.length > 0) {
+                          return getQuestionWidget();
+                        } else {
+                          return CircularProgressIndicator();
+                        }
+                      }))),
     );
-}
+  }
 
 /*
 Widget mainScreen(BuildContext context){
@@ -137,24 +128,50 @@ Widget mainScreen(BuildContext context){
    // );
   }
 */
-   Widget getQuestionWidget() {
-
-    switch(_getType(_currentQuestion++)){
+  Widget getQuestionWidget() {
+    switch (_getType(_currentQuestion++)) {
       case 0:
         return Column(children: <Widget>[
           Text("TextInputItem", textScaleFactor: 4),
+          Container(
+            margin: EdgeInsets.all(10),
+            width: MediaQuery.of(context).size.width / 3,
+            child: Draggable<Widget>(
+              child: Text('Text Input Field'),
+              data: createTextInputHandler,
+              feedback: Text('Text'),
+            ),
+          ),
           getNextButton()
         ]);
         break;
       case 1:
         return Column(children: <Widget>[
           Text("MultipleChoice", textScaleFactor: 4),
+          Container(
+            margin: EdgeInsets.all(10),
+            width: MediaQuery.of(context).size.width / 3,
+            child: Draggable<Widget>(
+              child: Text('Multiple Choice'),
+              data: createMultipleChoice,
+              feedback: Text('Mult choice'),
+            ),
+          ),
           getNextButton()
         ]);
         break;
       case 2:
         return Column(children: <Widget>[
           Text("ShortAnswer", textScaleFactor: 4),
+          Container(
+            margin: EdgeInsets.all(10),
+            width: MediaQuery.of(context).size.width / 3,
+            child: Draggable<Widget>(
+              child: Text('Short Answer'),
+              data: createShortAnswer,
+              feedback: Text('Short Answer'),
+            ),
+          ),
           getNextButton()
         ]);
         break;
@@ -181,26 +198,23 @@ Widget mainScreen(BuildContext context){
     }
   }
 
-
-Widget getNextButton(){
-      return RaisedButton(
-          child: Text("NEXT"),
-          color: Colors.red,
-          onPressed: () {
-            if(_currentQuestion < project.questions.length){
-                return getQuestionWidget();
-            }
-            return Text("All done!"); 
-            //setState(() {
-              
-              //_currentQuestion++;
-              //return getQuestionWidget();
-
-              //_getType(_currentQuestion);
-           // });
-
+  Widget getNextButton() {
+    return RaisedButton(
+        child: Text("NEXT"),
+        color: Colors.red,
+        onPressed: () {
+          if (_currentQuestion < project.questions.length) {
+            return getQuestionWidget();
           }
-      );
+          return Text("All done!");
+          //setState(() {
+
+          //_currentQuestion++;
+          //return getQuestionWidget();
+
+          //_getType(_currentQuestion);
+          // });
+        });
   }
 
   @override
@@ -208,25 +222,20 @@ Widget getNextButton(){
     project = new GetProject(docID: widget.docIDref, title: widget.title);
     //project.getdataFromProject();
     //_getQuestions();
-    projectFuture=_getQuestions();
+    projectFuture = _getQuestions();
     super.initState();
-    
   }
 
   Future<void> _getQuestions() async {
     // you mentioned you use firebase for database, so
     // you have to wait for the data to be loaded from the network
     return await project.getdataFromProject;
-   
-   
   }
 
   // Call this function when you want to move to the next page
   void goToNextPage() {
     //setState(() {
-      _currentQuestion++;
-   // });
+    _currentQuestion++;
+    // });
   }
-
-
 }
