404bfebdde2bb1643e6cbd0d711b948daf81c280
phadkem <51807611+phadkem@users.noreply.github.com>
2020-05-01 17:57:08 -0500

    web and chrome working

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import 'package:sensational_science/Services/auth.dart';
 import 'wrapper.dart';
 import 'models/user.dart';
+import 'package:firebase/firestore.dart';
 
 void main() {
   runApp(MyApp());
