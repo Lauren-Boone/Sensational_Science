9849633b7090eed070edfaed6d35f972afa8fda9
Abbi Devins-Suresh <aesther2012@gmail.com>
2020-05-01 16:04:41 -0400

    added back button to add project to class

--- lib/Screens/Teacher/addProjectToClass.dart
+++ lib/Screens/Teacher/addProjectToClass.dart
@@ -10,23 +10,30 @@ import 'package:provider/provider.dart';
 import 'package:sensational_science/models/user.dart';
 import 'package:sensational_science/Services/getproject.dart';
 
-class AddProjectToClass extends StatefulWidget{
+class AddProjectToClass extends StatefulWidget {
   AddProjectToClass({Key key}) : super(key: key);
   @override
   _AddProjectToClassState createState() => _AddProjectToClassState();
 }
 
-
-class _AddProjectToClassState extends State<AddProjectToClass>{
+class _AddProjectToClassState extends State<AddProjectToClass> {
   final _formKey = GlobalKey<FormState>();
   String _class;
   String _project;
   DateTime _date;
 
-  Future<void> assignProject (String uid, String className, String projectID, DateTime dueDate) async {
+  Future<void> assignProject(
+      String uid, String className, String projectID, DateTime dueDate) async {
     //create project under the class
-    DocumentReference classDoc = Firestore.instance.collection('Teachers').document(uid).collection('Classes').document(className);
-    final projectDoc = await Firestore.instance.collection('Projects').document(projectID).get();
+    DocumentReference classDoc = Firestore.instance
+        .collection('Teachers')
+        .document(uid)
+        .collection('Classes')
+        .document(className);
+    final projectDoc = await Firestore.instance
+        .collection('Projects')
+        .document(projectID)
+        .get();
     final projRef = await classDoc.collection('Projects').add({
       'projectID': projectID, //project doc id in top level project collection
       'projectTitle': projectDoc.data['title'], //project title
@@ -38,14 +45,23 @@ class _AddProjectToClassState extends State<AddProjectToClass>{
     //under top level codes collection
     final students = await classDoc.collection('Roster').getDocuments();
     for (var student in students.documents) {
-      var codeRef = await classDoc.collection('Projects').document(projRef.documentID).collection('Students').add({
+      var codeRef = await classDoc
+          .collection('Projects')
+          .document(projRef.documentID)
+          .collection('Students')
+          .add({
         'student': student.documentID, //student doc id in roster
         'completed': false, //has student submitted data
       });
       await classDoc.collection('Roster').document(student.documentID).setData({
-        'codes': FieldValue.arrayUnion([codeRef.documentID]) //list of codes for each student for all projects
+        'codes': FieldValue.arrayUnion([
+          codeRef.documentID
+        ]) //list of codes for each student for all projects
       }, merge: true);
-      await Firestore.instance.collection('codes').document(codeRef.documentID).setData({
+      await Firestore.instance
+          .collection('codes')
+          .document(codeRef.documentID)
+          .setData({
         'Teacher': uid, //teacher doc id
         'Class': className, //class doc id
         'Student': student.documentID, //student doc id in roster
@@ -58,33 +74,40 @@ class _AddProjectToClassState extends State<AddProjectToClass>{
   }
 
   @override
-
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
     return new MaterialApp(
       home: new Scaffold(
         appBar: new AppBar(
-          title: Text("Add Project To Class"),
-        ),
+            title: Text("Add Project To Class"),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () => Navigator.pop(context, false),
+            )),
         body: new Container(
           child: new Column(
             children: <Widget>[
               new StreamBuilder<QuerySnapshot>(
-                stream: Firestore.instance.collection('Teachers').document(user.uid)
-                  .collection('Classes').snapshots(),
+                stream: Firestore.instance
+                    .collection('Teachers')
+                    .document(user.uid)
+                    .collection('Classes')
+                    .snapshots(),
                 builder: (context, snapshot) {
-                  if(!snapshot.hasData) return Center(
-                    child: Text("Loading . . ."),
-                  );
+                  if (!snapshot.hasData)
+                    return Center(
+                      child: Text("Loading . . ."),
+                    );
                   return new Container(
                     padding: EdgeInsets.only(bottom: 10.0),
-                    width: MediaQuery.of(context).size.width*0.9,
+                    width: MediaQuery.of(context).size.width * 0.9,
                     child: new Row(
                       children: <Widget>[
                         new Expanded(
                           flex: 2,
                           child: new Container(
-                            padding: EdgeInsets.fromLTRB(12.0, 10.0, 10.0, 10.0),
+                            padding:
+                                EdgeInsets.fromLTRB(12.0, 10.0, 10.0, 10.0),
                             child: Text("Class to Assign Project For"),
                           ),
                         ),
@@ -109,17 +132,21 @@ class _AddProjectToClassState extends State<AddProjectToClass>{
                                   print(_class);
                                 });
                               },
-                              items: snapshot.data.documents.map((DocumentSnapshot document) {
+                              items: snapshot.data.documents
+                                  .map((DocumentSnapshot document) {
                                 return new DropdownMenuItem<String>(
                                   value: document.data['name'],
                                   child: new Container(
                                     decoration: new BoxDecoration(
                                       color: Colors.green,
-                                      borderRadius: new BorderRadius.circular(3.0),
+                                      borderRadius:
+                                          new BorderRadius.circular(3.0),
                                     ),
                                     height: 32.0,
-                                    width: MediaQuery.of(context).size.width*0.52,
-                                    padding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 0.0),
+                                    width: MediaQuery.of(context).size.width *
+                                        0.52,
+                                    padding: EdgeInsets.fromLTRB(
+                                        10.0, 5.0, 10.0, 0.0),
                                     child: Text(document.data['name']),
                                   ),
                                 );
@@ -135,18 +162,20 @@ class _AddProjectToClassState extends State<AddProjectToClass>{
               new StreamBuilder<QuerySnapshot>(
                 stream: Firestore.instance.collection('Projects').snapshots(),
                 builder: (context, snapshot) {
-                  if(!snapshot.hasData) return Center(
-                    child: Text('Loading . . .'),
-                  );
+                  if (!snapshot.hasData)
+                    return Center(
+                      child: Text('Loading . . .'),
+                    );
                   return new Container(
                     padding: EdgeInsets.only(bottom: 10.0),
-                    width: MediaQuery.of(context).size.width*0.9,
+                    width: MediaQuery.of(context).size.width * 0.9,
                     child: new Row(
                       children: <Widget>[
                         new Expanded(
                           flex: 2,
                           child: new Container(
-                            padding: EdgeInsets.fromLTRB(12.0, 10.0, 10.0, 10.0),
+                            padding:
+                                EdgeInsets.fromLTRB(12.0, 10.0, 10.0, 10.0),
                             child: Text("Project to Assign"),
                           ),
                         ),
@@ -171,17 +200,21 @@ class _AddProjectToClassState extends State<AddProjectToClass>{
                                   print(_project);
                                 });
                               },
-                              items: snapshot.data.documents.map((DocumentSnapshot document) {
+                              items: snapshot.data.documents
+                                  .map((DocumentSnapshot document) {
                                 return new DropdownMenuItem<String>(
                                   value: document.documentID,
                                   child: new Container(
                                     decoration: new BoxDecoration(
                                       color: Colors.green,
-                                      borderRadius: new BorderRadius.circular(3.0),
+                                      borderRadius:
+                                          new BorderRadius.circular(3.0),
                                     ),
                                     height: 32.0,
-                                    width: MediaQuery.of(context).size.width*0.52,
-                                    padding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 0.0),
+                                    width: MediaQuery.of(context).size.width *
+                                        0.52,
+                                    padding: EdgeInsets.fromLTRB(
+                                        10.0, 5.0, 10.0, 0.0),
                                     child: Text(document.data['title']),
                                   ),
                                 );
@@ -195,8 +228,10 @@ class _AddProjectToClassState extends State<AddProjectToClass>{
                 },
               ),
               Container(
-                padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.1, bottom: 10.0),
-                width: MediaQuery.of(context).size.width*0.9,
+                padding: EdgeInsets.only(
+                    left: MediaQuery.of(context).size.width * 0.1,
+                    bottom: 10.0),
+                width: MediaQuery.of(context).size.width * 0.9,
                 child: DateTimeField(
                   format: DateFormat("yyyy-MM-dd"),
                   decoration: const InputDecoration(
@@ -240,25 +275,23 @@ class _AddProjectToClassState extends State<AddProjectToClass>{
                       );
                     }
                     showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: Text("Success!"),
-                          content: Text(
-                              "A project has been added to the class. View the class details to see the assigned project."),
-                          actions: <Widget>[
-                            RaisedButton(
-                              child: Text("Close"),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                            )
-                          ]
-                        );
-                      }
-                    );  
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                              title: Text("Success!"),
+                              content: Text(
+                                  "A project has been added to the class. View the class details to see the assigned project."),
+                              actions: <Widget>[
+                                RaisedButton(
+                                  child: Text("Close"),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                )
+                              ]);
+                        });
                     Navigator.pop(context);
-                    return;       
+                    return;
                   },
                   child: Text('Add Project'),
                 ),
