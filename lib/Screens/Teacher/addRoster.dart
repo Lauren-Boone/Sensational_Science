fd88cba5923fff494c1dd050427d0d338f38dd75
phadkem <51807611+phadkem@users.noreply.github.com>
2020-05-03 20:14:15 -0500

    View & add roster merged to one page

--- lib/Screens/Teacher/addRoster.dart
+++ lib/Screens/Teacher/addRoster.dart
@@ -20,38 +20,47 @@ class _AddRosterState extends State<AddRoster>{
   String name;
   List<DynamicWidget> roster=[];
 
-addStudent(){
-  
-  roster.add(new DynamicWidget());
-  setState((){
-
-  });
-}
+  addStudent(){
+    roster.add(new DynamicWidget());
+    setState((){
+    });
+  }
 
  submitData(teachID) async {
   //String val='Success!';
-roster.forEach((e){
-  Firestore.instance
-  .runTransaction((transaction) async{
-    await transaction.set(Firestore.instance
-    .collection("Teachers")
-  .document(teachID)
-  .collection('Classes')
-  .document(widget.name)
-  .collection('Roster')
-  .document(),
-  {
-    
-      'name': e.controller.text,
-  
-    
-  
+  roster.forEach((e){
+    Firestore.instance
+      .runTransaction((transaction) async{
+        await transaction.set(Firestore.instance
+          .collection("Teachers")
+          .document(teachID)
+          .collection('Classes')
+          .document(widget.name)
+          .collection('Roster')
+          .document(),
+          {
+            'name': e.controller.text,
+          }
+        );
+      }
+    ); 
   });
-  });
-
-print(teachID);
-  });
- // return val;
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Students added!'),
+        content: Text('Students have been added to the class'),
+        actions: <Widget>[
+          RaisedButton(child: Text("Close"),
+            onPressed: () {Navigator.of(context).pop();},
+          ),
+        ]
+      );
+    },
+  );
+  roster = [];
+  setState(() {});
 }
 
   /*
@@ -85,40 +94,65 @@ String success = '';
     return new MaterialApp(
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text("Add Roster"),
+          title: new Text("View & Add To Roster"),
         ),
         body: new Container(
-          child: new Column(
-            children: <Widget>[
-              new Flexible(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height * 0.8,
+          child: Column(
+            children: [
+              new Text('Current Roster'),
+              new StreamBuilder(
+                stream: Firestore.instance.collection('Teachers').
+                  document(user.uid)
+                  .collection('Classes')
+                  .document(widget.name)
+                  .collection('Roster')
+                  .snapshots(),
+                builder: (BuildContext context, snapshot) {
+                  if(!snapshot.hasData) return new Text('...Loading');
+                  return new Expanded(
+                    child: SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.8,
+                      child: new ListView(
+                        children: snapshot.data.documents.map<Widget>((doc){
+                          return new ListTile(
+                            title: new Text(doc['name']),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  );
+                },
+              ),
+              new Divider(
+                color: Colors.blue,
+                height: 10.0,
+              ),
+              new Text('Students to add:'),
+              new Expanded(
                 child: new ListView.builder(
                   itemCount: roster.length,
                   itemBuilder: (_, index)=>roster[index]
-
-                  ),
                 ),
-                 new Container(
-                  child: Text(success),
-                ),
+              ),
                 new Container(
-                  child: new RaisedButton(
-                    child: new Text('Submit'),
-                    onPressed: ()=> 
-                    
-                       submitData(user.uid),
-                        
-                    
-                    ),
-                     
-                    
-                ),
-               
+                child: Text(success),
+              ),
+              new RaisedButton(
+                child: new Text('Submit Students'),
+                onPressed: ()=> submitData(user.uid),
+              ),
+              new RaisedButton(
+                child: new Text('Add Another Student'),
+                onPressed: addStudent,
+              ),
             ],
           ),
         ),
-        floatingActionButton: new FloatingActionButton(
-          onPressed: addStudent,
-          child: new Icon(Icons.add),
+        floatingActionButton: new RaisedButton(
+          onPressed: () { Navigator.pop(context);},
+          child: Text('Go Back'),
         ),
       ),
     );
