f36479cb6c3d5ff6cc78780767b3b8af509bb28f
Abbi Devins-Suresh <aesther2012@gmail.com>
2020-05-06 16:11:06 -0400

    working on uploading to db

--- lib/Screens/Student/saveStudentAnswers.dart
+++ lib/Screens/Student/saveStudentAnswers.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -9,3 +10,10 @@ import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:provider/provider.dart';
 import 'package:sensational_science/models/user.dart';
 
+//references: https://medium.com/47billion/how-to-use-firebase-with-flutter-e4a47a7470ce
+//https://stackoverflow.com/questions/52072948/flutter-get-firebase-database-reference-child-all-data/52073274
+
+var databaseReference = FirebaseDatabase.instance.reference().child("Students"); 
+void saveAnswers(){
+  
+}
