192ee3325ebd0c0551350cd1546ae40fa3a7f31a
Abbi Devins-Suresh <aesther2012@gmail.com>
2020-05-06 17:00:13 -0400

    added class

--- lib/Screens/Student/saveStudentAnswers.dart
+++ lib/Screens/Student/saveStudentAnswers.dart
@@ -13,7 +13,27 @@ import 'package:sensational_science/models/user.dart';
 //references: https://medium.com/47billion/how-to-use-firebase-with-flutter-e4a47a7470ce
 //https://stackoverflow.com/questions/52072948/flutter-get-firebase-database-reference-child-all-data/52073274
 
-var databaseReference = FirebaseDatabase.instance.reference().child("Students"); 
+var databaseReference = FirebaseDatabase.instance.reference().child("Teachers").child("WIWJTyjBw9WwfUfNQ69D6ABVOx93")
+.child("Classes").child("AbbiTest").child("Projects").child("MMxdCXvncfz8VFNPlc5q").child("Students"); 
 void saveAnswers(){
-  
+  databaseReference.child("Answers").set({
+    'example': 'test'
+  });
+}
+
+class saveStudentAnswers extends StatefulWidget{
+    saveStudentAnswers({Key key}) : super(key: key);
+
+  @override
+  saveStudentAnswersState createState() => saveStudentAnswersState();
+}
+
+class saveStudentAnswersState extends State<saveStudentAnswers>{
+  @override
+
+  Widget build(BuildContext context){
+    return Scaffold(
+
+    );
+  }
 }
