4a3b0e7573525caf17e2194a45c60ed7dffbb6e2
phadkem <51807611+phadkem@users.noreply.github.com>
2020-05-08 13:44:25 -0500

    add classInfo model

---
+++ lib/models/classInfo.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ClassInfo{
+  final String classID;
+  final String teachID;
+  String className;
+  String classLevel;
+  int projectCount;
+  String school;
+  int studentCount;
+  String subject;
+
+  ClassInfo({this.classID, this.teachID}) {
+    final classDoc = Firestore.instance.collection('Teachers').document(teachID).collection('Classes').document(classID).get();
+    classDoc.then( (doc) {
+      className = doc.data['name'];
+      classLevel = doc.data['level'];
+      projectCount = doc.data['projects'];
+      school = doc.data['school'];
+      studentCount = doc.data['students'];
+      subject = doc.data['subject'];
+    });
+  }
+}
