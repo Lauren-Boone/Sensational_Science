d7c460bd7716dc8cac128dbc5a34cbb2cb817d04
Abbi Devins-Suresh <aesther2012@gmail.com>
2020-05-13 21:01:19 -0400

    fixed api key issue and map loading

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,16 +5,35 @@ import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    FirebaseApp.configure()
-    RemoteConfig.remoteConfig().fetchAndActivate(){status, error in
-      let googleMapsApiKey : String = RemoteConfig.remoteConfig()["google_maps_api"].stringValue ?? "MISSING"; 
-      GMSServices.provideAPIKey(googleMapsApiKey)
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        FirebaseApp.configure()
+
+        var keys: NSDictionary?
+
+        if let path = Bundle.main.path(forResource: "keys", ofType: "plist"){
+          keys = NSDictionary(contentsOfFile: path)
+        }
+
+        if let dictionary = keys{
+            let apiKey = dictionary["google_maps_api"] as? String
+            GMSServices.provideAPIKey(apiKey ?? "MISSING")
+        }
+//        let remoteConfig = RemoteConfig.remoteConfig()
+//        let settings = RemoteConfigSettings(developerModeEnabled: true)
+//        settings.minimumFetchInterval = 0
+//
+//        remoteConfig.configSettings = settings
+//        remoteConfig.fetch(withExpirationDuration: 0.1){status, error in
+//            print("IN REMOTE CONFIG");
+//            let googleMapsApiKey : String = remoteConfig["google_maps_api"].stringValue ?? "MISSING";
+//            print(googleMapsApiKey);
+//            GMSServices.provideAPIKey("AIzaSyAPt4bVD-IWPFEhebtDfqoyejghOGFDFm0")
+//        }
+        // GMSServices.provideAPIKey("AIzaSyAPt4bVD-IWPFEhebtDfqoyejghOGFDFm0")
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
 }
