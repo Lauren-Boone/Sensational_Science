e3162fab7c1a8ac1f11f00e05e1fad75dd73a24a
Abbi Devins-Suresh <aesther2012@gmail.com>
2020-05-14 18:45:28 -0400

    gets location from answer for map

--- lib/Screens/Student/locationtest.dart
+++ lib/Screens/Student/locationtest.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:sensational_science/Screens/Student/student_view_class_data.dart';
+import 'package:sensational_science/Screens/Student/student_view_class_data.dart';
 import 'package:sensational_science/Screens/Teacher/FormInputs/userlocation.dart';
 import 'package:sensational_science/Services/projectDB.dart';
 import '../../models/project.dart';
@@ -7,7 +9,7 @@ import '../../Services/getproject.dart';
 
 Map<MarkerId, Marker> marker = <MarkerId, Marker>{};
 
-void add(){
+void add(lat, lon){
   final String markerIdValue = "1"; 
   final MarkerId markerId = MarkerId(markerIdValue); 
   final Marker marker = Marker(
@@ -17,8 +19,10 @@ void add(){
 }
 class LocationMap extends StatefulWidget {
   final TextEditingController controller;
+  final locationInfo lms;
+  // final List<String> locations;
 
-  LocationMap({this.controller});
+  LocationMap({this.controller, this.lms});
 
   @override
   LocationMapState createState() => LocationMapState();
@@ -45,7 +49,19 @@ class LocationMapState extends State<LocationMap> {
   //           )));
   // }
   Widget build(BuildContext context){
+    // print(locationInfo); 
+    print("in location" + widget.lms.toString());
+    RegExp exp = new RegExp(r"^Lat: (.*), Long: (.*)");
+    var lms_actual = widget.lms.toString();
+    var lms_test = widget.lms.latlonInfo; 
+    print(lms_actual); 
+    Iterable<RegExpMatch> matches = exp.allMatches(lms_test);
+        print(matches.toList());
+    double lat = double.parse(matches.toList()[0].group(1));
+    double lon = double.parse(matches.toList()[0].group(2));
+
                 return Material(
+                  
                       child: new Container(
               child: Column(
                 children: <Widget>[
@@ -56,7 +72,7 @@ class LocationMapState extends State<LocationMap> {
                       width: MediaQuery.of(context).size.width/3, 
                       child: GoogleMap(
               mapType: MapType.hybrid,
-              initialCameraPosition:CameraPosition(target: LatLng(43.6591, -70.2568), zoom: 11),
+              initialCameraPosition:CameraPosition(target: LatLng(lat, lon), zoom: 11),
              
                     ),
                     )],
