e5b281e9102ec7900b3c69ee09da482c0f7f252f
Abbi Devins-Suresh <aesther2012@gmail.com>
2020-05-16 15:10:14 -0400

    got previous button bugs fixed

--- lib/Screens/Student/student_view_class_data.dart
+++ lib/Screens/Student/student_view_class_data.dart
@@ -11,7 +11,7 @@ var createLocationMap = LocationMap();
 class locationInfo {
   // double latitude = 0.0;
   // double longitude = 0.0;
-  String latlonInfo; 
+  String latlonInfo;
 
   locationInfo({this.latlonInfo});
 }
@@ -53,8 +53,11 @@ class _ViewClassDataState extends State<ViewClassData> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("View Data"),
-      ),
+          title: Text("View Data"),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () => Navigator.pop(context, false),
+          )),
       body: Container(
         margin: EdgeInsets.all(40),
         child: Center(
@@ -62,7 +65,8 @@ class _ViewClassDataState extends State<ViewClassData> {
             Padding(
               padding: EdgeInsets.all(20),
             ),
-            Text("All of the students' answers have been compiled. ", style: TextStyle(color: Colors.black, fontSize: 20) ),
+            Text("All of the students' answers have been compiled. ",
+                style: TextStyle(color: Colors.black, fontSize: 20)),
             Padding(
               padding: EdgeInsets.all(20),
             ),
@@ -201,39 +205,36 @@ class _CompileDataState extends State<CompileData> {
         child: Text("Prev"),
         color: Colors.red,
         onPressed: () {
-          if (_currentQuestion < widget.proj.questions.length) {
+          if (_currentQuestion < widget.proj.questions.length &&
+              _currentQuestion != 0) {
             setState(() {
               //controllers.add(value);
               _currentQuestion--;
             });
-          } else {}
+          } else if (_currentQuestion == 0) {
+            Navigator.pop(context);
+          }
         });
   }
 
   Widget build(BuildContext context) {
-   
     if (_currentQuestion >= widget.proj.questions.length) {
-      
       return Material(
         child: Container(
-          margin: EdgeInsets.all(30),
-          color: Colors.white,
-          child: Column(
-            children: <Widget>[
-              Text('End of Compiled Answers'),
-              RaisedButton(
-                child: Text('Click to Go back'),
-                onPressed: ()=>
-                  Navigator.of(context).pop(),
-                  
-              )
-            ],
-          )
-        ),
-        
+            margin: EdgeInsets.all(30),
+            color: Colors.white,
+            child: Column(
+              children: <Widget>[
+                Text('End of Compiled Answers'),
+                RaisedButton(
+                  child: Text('Click to Go back'),
+                  onPressed: () => Navigator.of(context).pop(),
+                )
+              ],
+            )),
       );
     }
-     while (proj.questions[_currentQuestion].compAnswers.length == 0) {
+    while (proj.questions[_currentQuestion].compAnswers.length == 0) {
       setState(() {});
     }
     switch (widget.proj.questions[_currentQuestion].type.toString()) {
@@ -253,12 +254,13 @@ class _CompileDataState extends State<CompileData> {
                           .proj.questions[_currentQuestion].compAnswers[index]);
                       return ListTile(
                         title: Text(
-                            '${widget.proj.questions[_currentQuestion].compAnswers[index]}',
+                            'Location Map',
                             style: TextStyle(color: Colors.black)),
                       );
                     },
                   ),
                 ),
+                getPrevButton(context),
                 getNextButton(context),
               ],
             ),
@@ -353,25 +355,26 @@ class _CompileDataState extends State<CompileData> {
 
         break;
       case 'UserLocation':
-      print('${widget.proj.questions[_currentQuestion].compAnswers[0]}');
-      var locationInfoMap = widget
-                           .proj.questions[_currentQuestion].compAnswers[0];
-                           final lms = locationInfo(latlonInfo: '${widget.proj.questions[_currentQuestion].compAnswers[0]}');
+        // print('${widget.proj.questions[_currentQuestion].compAnswers[0]}');
+        var locationInfoMap =
+            widget.proj.questions[_currentQuestion].compAnswers[0];
+        final lms = locationInfo(
+            latlonInfo:
+                '${widget.proj.questions[_currentQuestion].compAnswers[0]}');
         return Material(
-          
           child: new Container(
             child: Column(
               children: <Widget>[
                 Expanded(
                     child: Text(
-                        'compiledataclass${widget.proj.questions[_currentQuestion].compAnswers[0]}')),
+                        '')),
                 Container(
                   height: MediaQuery.of(context).size.height / 3,
                   width: MediaQuery.of(context).size.width / 3,
                   child: new LocationMap(lms: lms),
                   // child: RaisedButton(
                   //   child: Text('Click to load map'),
-                    
+
                   //   onPressed: () {
                   //     // var locationInfoMap = createLocationMap;
                   //     var locationInfoMap = widget
@@ -419,7 +422,7 @@ class _CompileDataState extends State<CompileData> {
                     animate: true,
                     animationDuration: Duration(seconds: 2),
                     // barRendererDecorator: new charts.BarLabelDecorator<String>(),
-      domainAxis: new charts.OrdinalAxisSpec(),
+                    domainAxis: new charts.OrdinalAxisSpec(),
                     behaviors: [
                       new charts.DatumLegend(
                         outsideJustification:
@@ -435,10 +438,9 @@ class _CompileDataState extends State<CompileData> {
                         ),
                       ),
                     ],
-                    
+
                     defaultRenderer: new charts.BarRendererConfig(
                       groupingType: charts.BarGroupingType.groupedStacked,
-                      
                     ),
                   ),
                 ),
