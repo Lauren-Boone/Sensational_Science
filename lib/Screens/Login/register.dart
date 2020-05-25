ee8b047e5ed0519f1ffecf6d4050a93d437defd7
phadkem <51807611+phadkem@users.noreply.github.com>
2020-05-25 09:55:08 -0500

    change log in pages to be scrollable

--- lib/Screens/Login/register.dart
+++ lib/Screens/Login/register.dart
@@ -35,75 +35,77 @@ String error='';
           ),
         ],
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20),
-               TextFormField(
-                  validator: (val)=> val.length < 6 ? 'Enter Name' : null,
-                   decoration: const InputDecoration(
-                  hintText: 'Full Name',
-                ),
-                  obscureText: false,
-                  onChanged: (val){
-                    setState(()=>name=val);
-                  }
-                  
-                ),
-              TextFormField(  
-                validator: (val)=> val.isEmpty ? 'Enter an email' : null,
-                 decoration: const InputDecoration(
-                  hintText: 'Email',
-                ),            
-                onChanged: (val){
-                  setState(()=> email = val);
-
-                },
-                ),
-                SizedBox(height: 20),
-                TextFormField(
-                  validator: (val)=> val.length < 6 ? 'Enter a password 6+ chars long' : null,
-                   decoration: const InputDecoration(
-                  hintText: 'Password',
-                ),
-                  obscureText: true,
-                  onChanged: (val){
-                    setState(()=>password=val);
-                  }
-                  
-                ),
-               
-                SizedBox(height: 20),
-                RaisedButton(
-                  color: Colors.blue[200],
-                  child: Text(
-                    'Create Account',
-                  ),
-                  onPressed: () async{
-                    if(_formKey.currentState.validate()){
-                      dynamic result = await _auth.register(email.toString().trim(), password, name);
-                      print(email);
-                      if(result == null){
-                        setState(()=> error = 'please supply a valid email');
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 20),
+                  TextFormField(
+                      validator: (val)=> val.length < 6 ? 'Enter Name' : null,
+                      decoration: const InputDecoration(
+                      hintText: 'Full Name',
+                    ),
+                      obscureText: false,
+                      onChanged: (val){
+                        setState(()=>name=val);
                       }
-                    }
+                      
+                    ),
+                  TextFormField(  
+                    validator: (val)=> val.isEmpty ? 'Enter an email' : null,
+                    decoration: const InputDecoration(
+                      hintText: 'Email',
+                    ),            
+                    onChanged: (val){
+                      setState(()=> email = val);
 
-                  }
-                ),
-                 
-                SizedBox(height: 12),
-                Text(
-                  error,
+                    },
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      validator: (val)=> val.length < 6 ? 'Enter a password 6+ chars long' : null,
+                      decoration: const InputDecoration(
+                      hintText: 'Password',
+                    ),
+                      obscureText: true,
+                      onChanged: (val){
+                        setState(()=>password=val);
+                      }
+                      
+                    ),
                   
-                ),
-                   
-            ]
-          ),
-          ),
-      ),
+                    SizedBox(height: 20),
+                    RaisedButton(
+                      color: Colors.blue[200],
+                      child: Text(
+                        'Create Account',
+                      ),
+                      onPressed: () async{
+                        if(_formKey.currentState.validate()){
+                          dynamic result = await _auth.register(email.toString().trim(), password, name);
+                          print(email);
+                          if(result == null){
+                            setState(()=> error = 'please supply a valid email');
+                          }
+                        }
+
+                      }
+                    ),
+                    
+                    SizedBox(height: 12),
+                    Text(
+                      error,
+                      
+                    ),
+                      
+                ]
+              ),
+              ),
+        ),
+    ),
 
       
     );
--- lib/Screens/Login/sign_in.dart
+++ lib/Screens/Login/sign_in.dart
@@ -25,107 +25,109 @@ class _SignInState extends State<SignIn> {
   String error='';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.green[200],
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
-        title: Text('Log In'),
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Register'),
-            onPressed: () => Navigator.push(
-                      context, MaterialPageRoute(
-                        builder: (context) => CreateAccount()
-                      ),
-            )
-          ),
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-        child: Column(
-          children: <Widget>[
-            RaisedButton(
-              color: Colors.blue[400],
-              child: Text('Access Project Using Student Code'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => StudentEnterCode()
-                  ),
-                );
-              },
-            ),
-            Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 20),
-                TextFormField(  
-                  validator: (val)=> val.isEmpty ? 'Enter an email' : null,
-                  decoration: const InputDecoration(
-                    hintText: 'Email',
-                  ),            
-                  onChanged: (val){
-                    setState(()=> email = val);
-
-                  },
-                  ),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    validator: (val)=> val.length < 6 ? 'Must be 6 characters' : null,
-                    decoration: const InputDecoration(
-                    hintText: 'Password',
-                  ),
-                    obscureText: true,
-                    onChanged: (val){
-                      setState(()=>password=val);
-                    }
-                    
-                  ),
-                  SizedBox(height: 20),
-                  RaisedButton(
-                    color: Colors.blue[400],
-                    child: Text(
-                      'Sign In',
-                    ),
-                    onPressed: () async{
-                      if(_formKey.currentState.validate()){
-                      // setState(() => loading = true);
-                        dynamic result = await _auth.signIn(email.toString().trim(), password);
-                        print(email);
-                        if(result == null){
-                          loading = false;
-                          setState(()=> error = 'please supply a valid email');
-                        }
-                      }
-
-                    }
-                  ), 
-                  // RaisedButton(
-                  //   child: Text(
-                  //     'Create Account',
-                  //   ), onPressed: (){
-                  //     Navigator.push(
-                  //       context, MaterialPageRoute(
-                  //         builder: (context) => CreateAccount()
-                  //       )
-                  //     ); 
-                  //   }
-
-                  //   )
-                  
-                  
-              ],
-            ),
+      return Scaffold(
+        backgroundColor: Colors.green[200],
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple,
+          title: Text('Log In'),
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text('Register'),
+              onPressed: () => Navigator.push(
+                        context, MaterialPageRoute(
+                          builder: (context) => CreateAccount()
+                        ),
+              )
             ),
           ],
         ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+            child: Column(
+              children: <Widget>[
+                RaisedButton(
+                  color: Colors.blue[400],
+                  child: Text('Access Project Using Student Code'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => StudentEnterCode()
+                      ),
+                    );
+                  },
+                ),
+                Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 20),
+                    TextFormField(  
+                      validator: (val)=> val.isEmpty ? 'Enter an email' : null,
+                      decoration: const InputDecoration(
+                        hintText: 'Email',
+                      ),            
+                      onChanged: (val){
+                        setState(()=> email = val);
+
+                      },
+                      ),
+                      SizedBox(height: 20),
+                      TextFormField(
+                        validator: (val)=> val.length < 6 ? 'Must be 6 characters' : null,
+                        decoration: const InputDecoration(
+                        hintText: 'Password',
+                      ),
+                        obscureText: true,
+                        onChanged: (val){
+                          setState(()=>password=val);
+                        }
+                        
+                      ),
+                      SizedBox(height: 20),
+                      RaisedButton(
+                        color: Colors.blue[400],
+                        child: Text(
+                          'Sign In',
+                        ),
+                        onPressed: () async{
+                          if(_formKey.currentState.validate()){
+                          // setState(() => loading = true);
+                            dynamic result = await _auth.signIn(email.toString().trim(), password);
+                            print(email);
+                            if(result == null){
+                              loading = false;
+                              setState(()=> error = 'please supply a valid email');
+                            }
+                          }
+
+                        }
+                      ), 
+                      // RaisedButton(
+                      //   child: Text(
+                      //     'Create Account',
+                      //   ), onPressed: (){
+                      //     Navigator.push(
+                      //       context, MaterialPageRoute(
+                      //         builder: (context) => CreateAccount()
+                      //       )
+                      //     ); 
+                      //   }
+
+                      //   )
+                      
+                      
+                  ],
+                ),
+                ),
+              ],
+            ),
+          ),
       ),
 
-      
-    );
+        
+      );
   }
 }
