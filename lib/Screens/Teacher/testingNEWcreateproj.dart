a04757123abd6e7ec82e6c360fba090e31236a9c
Lauren Boone <lboone2094@gmail.com>
2020-05-28 15:54:42 -0600

    add remove question function

--- lib/Screens/Teacher/testingNEWcreateproj.dart
+++ lib/Screens/Teacher/testingNEWcreateproj.dart
@@ -37,28 +37,38 @@ class AddQuestionsToProject extends StatefulWidget {
 }
 
 //List<TextEditingController> questions = new List<TextEditingController>();
-
+int numQuestions = 0;
+List<ItemData> items = [];
+  List<String> typecontroller=[];
+  List<TextEditingController> questions=[];
  List<DynamicWidget> questionwidgets = [];
  _removeQuestion(int number){
-   questionwidgets.removeAt(number);
+   questionwidgets.removeAt(number-1);
+   typecontroller.removeAt((number-1));
+   //questions.removeAt((number-1));
+   //items.removeAt(number-1);
+   numQuestions--;
+   for(int i = (number-1); i<questionwidgets.length; ++i){
+     questionwidgets[i].numq = i+1;
+   }
+   
  }
 List<List<TextEditingController>> answers = [];
  _removeAnswers(int number){
-   answers.removeAt(number);
+  questionwidgets[number-1].answers.removeAt(number-1);
+  
  }
-
+bool needStateChange = false;
 class _AddQuestionsToProjectState extends State<AddQuestionsToProject> {
   //List<DynamicWidget> addQuestiontoAccordion = new List();
  
-  List<ItemData> items = [];
-  List<String> typecontroller=[];
-  List<TextEditingController> questions=[];
+  
   //List<List<TextEditingController>> answers = [];
   List<String> types = ["MultipleChoice", "TextInputItem", "ShortAnswerItem", "UserLocation", "NumericalInputItem", "AddImageInput"];
   String curVal='MultipleChoice';
 
   //String selected;
-  int numQuestions = 0;
+  
 bool _checkforInput(){
   bool retVal = true;
   if(questionwidgets.length==0){
@@ -82,8 +92,15 @@ bool _checkforInput(){
   });
   return retVal;
 }
+bool reset = false;
+callback(reset) {
+        setState(() {
+           needStateChange=false;
+        });
+      }
   @override
   Widget build(BuildContext context) {
+    
      final user = Provider.of<User>(context);
     return Material(
       child: Scaffold(
@@ -131,6 +148,7 @@ bool _checkforInput(){
               child: new ListView.builder(
                   itemCount: questionwidgets.length,
                   itemBuilder: (_, index) => questionwidgets[index]),
+                  
             ),
             new RaisedButton(
               child: new Text('Add a Question'),
@@ -187,7 +205,7 @@ bool _checkforInput(){
                                   numQuestions++;
                                   setState(() {
                                    questionwidgets.add(new DynamicWidget(
-                                      type: curVal, numq: numQuestions));
+                                      type: curVal, numq: numQuestions, callback: callback));
                                       typecontroller.add(curVal);
                                   });
                              
@@ -310,11 +328,13 @@ class DynamicWidget extends StatefulWidget {
   final controller = new TextEditingController();
   // final answercontroller = new List<TextEditingController>();
   final answers = new List<DynamicAnswers>();
+  final Function(bool) callback;
+
   int numAnswers = 0;
   final String type;
-  final int numq;
+   int numq;
 
-  DynamicWidget({this.type, this.numq});
+  DynamicWidget({this.type, this.numq, this.callback});
   @override
   _DynamicWidgetState createState() => _DynamicWidgetState();
 }
@@ -354,8 +374,12 @@ class _DynamicWidgetState extends State<DynamicWidget> {
         new RaisedButton(
           child: Text("Remove question"),
           onPressed: ()=>{
+            
               _removeQuestion(widget.numq),
-              _removeAnswers(widget.numq),
+              //_removeAnswers(widget.numq),
+              needStateChange=true,
+              widget.callback(true),
+              setState((){}),
           },
         )
       ],
@@ -380,7 +404,10 @@ class _DynamicWidgetState extends State<DynamicWidget> {
           child: Text("Remove question"),
           onPressed: ()=>{
               _removeQuestion(widget.numq),
+              needStateChange=true,
+              widget.callback(true),
              // _removeAnswers(widget.numq),
+             setState((){}),
           },
         )
           ],
